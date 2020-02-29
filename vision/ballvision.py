2625dc19c8f327d78f4f4e5f18397347a1bcf1ff
unknown <57297334+A83RS@users.noreply.github.com>
2020-02-29 10:45:26 -0700

    can connect to camera

--- vision/ballvision.py
+++ vision/ballvision.py
@@ -2,8 +2,8 @@
 #BALL SLURPER
 # This example shows off single color RGB565 tracking using the OpenMV Cam.
 
-import sensor, pin, image, time, math, ustruct, pyb
-from pyb import USB_VCP
+import sensor, image, time, math,ustruct, pyb
+from pyb import USB_VCP, Pin
 
 DBG=True
 
@@ -18,7 +18,7 @@ sensor.reset()
 
 sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QVGA)
-sensor.set_auto_exposure(False, 5000)
+sensor.set_auto_exposure(False)
 sensor.set_auto_gain(False) # must be turned off for color tracking
 sensor.set_auto_whitebal(False) # must be turned off for color tracking
 sensor.skip_frames(time = 2000)
@@ -35,17 +35,16 @@ def angle(ball):
         return px * .22125
 
 
-stream = False
+stream = True
 
 while(True):
     clock.tick()
     img = sensor.snapshot()
     ball_angle = 0
-    ball_dist = 0
-    tracking_ball = False
+    tracking_ball = 0
     ball = None
-
-    for blob in img.find_blobs([(30, 100, -50, 20, 35, 75)], pixels_threshold=100, area_threshold=100, merge=True):
+    ball_distance = 0
+    for blob in img.find_blobs([(45, 100, -27, 6, 41, 127)], pixels_threshold=100, area_threshold=100, merge=True):
         # These values depend on the blob not being circular - otherwise they will be shaky.
         if (ball == None ):
             ball = blob
@@ -53,17 +52,17 @@ while(True):
         elif( blob.cy() > ball.cy() ):
             ball = blob
         # These values are stable all the time.
-        img.draw_rectangle(blob.rect())
-        img.draw_cross(blob.cx(), blob.cy())
-    # Note - the blob rotation is unique to 0-180 only.
+        if (stream):
+            img.draw_rectangle(blob.rect())
+            img.draw_cross(blob.cx(), blob.cy())
+        # Note - the blob rotation is unique to 0-180 only.
     if ( ball != None ):
-        img.draw_rectangle(ball.rect(), (0,0,255))
         ball_angle = (angle(ball))
-        tracking_ball = True
-    print( tracking_ball)
+        tracking_ball = 1
+        if (stream):
+            img.draw_rectangle(ball.rect(), (0,0,255))
 
-
-    # \/ \/ USB CODE \/ \/
+    # \/ \/ USB CODE \/ \/ AND THE LLLL's are attacking!
     ticks = time.ticks()
     if stream:
         img.copy(x_scale=.25,y_scale=.25,copy_to_fb=True)
@@ -73,10 +72,10 @@ while(True):
     if not cmd:
         continue
     if cmd[0] == b's'[0] and stream:
-        usb.send(ustruct.pack(">llll", tracking_ball , ball_angle , ball_dist, img.size()))
+        usb.send(ustruct.pack(">llll", tracking_ball , ball_angle , ball_distance , img.size()))
         usb.send(img)
     else:
-        usb.send(ustruct.pack(">llll", tracking_ball , ball_angle , ball_dist, 0))
+        usb.send(ustruct.pack(">llll", tracking_ball , ball_angle , ball_distance , 0 ))
         if cmd[0] == b's'[0]:
             stream = True
         else:
@@ -88,7 +87,8 @@ while(True):
     if cmd[1] == b'g'[0]:
         pyb.LED(2).toggle()
     if cmd[1] == b'w'[0]:
-        pin.value(False)
-    else:
         pin.value(True)
+    else:
+        pin.value(False)                                  
+           # /\ /\ USB CODE /\ /\
     # /\ /\ USB CODE /\ /\
