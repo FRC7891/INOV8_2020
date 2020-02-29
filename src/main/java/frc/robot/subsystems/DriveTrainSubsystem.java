c0f55bbd550e917a00d446d67b900f734f41cf4c
Broydeen <braden.s3@hotmail.com>
2020-02-29 13:12:45 -0700

    Uncommented PID code

--- src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -34,114 +34,112 @@ public class DriveTrainSubsystem extends SubsystemBase {
   double SensorTurnValue;
   public DriveTrainSubsystem() {
 
-		MotorR1.configOpenloopRamp(0.9,0);
+		MotorR1.configOpenloopRamp(0.9,0);		
 		MotorL1.configOpenloopRamp(0.9,0);
+// Invert Directions for Left and Right
+TalonFXInvertType _leftInvert = TalonFXInvertType.CounterClockwise; //Same as invert = "false"
+TalonFXInvertType _rightInvert = TalonFXInvertType.Clockwise; //Same as invert = "true"
 
-///** Invert Directions for Left and Right */
-//TalonFXInvertType _leftInvert = TalonFXInvertType.CounterClockwise; //Same as invert = "false"
-//TalonFXInvertType _rightInvert = TalonFXInvertType.Clockwise; //Same as invert = "true"
-//
-///** Config Objects for motor controllers */
-//TalonFXConfiguration _leftConfig = new TalonFXConfiguration();
-//TalonFXConfiguration _rightConfig = new TalonFXConfiguration();
-//
-///* Set Neutral Mode */
-//MotorL1.setNeutralMode(NeutralMode.Brake);
-//MotorR1.setNeutralMode(NeutralMode.Brake);
-//
-///* Configure output and sensor direction */
-//MotorL1.setInverted(_leftInvert);
-//MotorR1.setInverted(_rightInvert);
-//
-//
-///* Reset Pigeon Configs */
-//_pidgey.configFactoryDefault();
-//
-//
-///** Feedback Sensor Configuration */
-//
-///** Distance Configs */
-//
-///* Configure the left Talon's selected sensor as integrated sensor */
-//_leftConfig.primaryPID.selectedFeedbackSensor = FeedbackDevice.IntegratedSensor; //Local Feedback Source
-//
-///* Configure the Remote (Left) Talon's selected sensor as a remote sensor for the right Talon */
-//_rightConfig.remoteFilter0.remoteSensorDeviceID = MotorL1.getDeviceID(); //Device ID of Remote Source
-//_rightConfig.remoteFilter0.remoteSensorSource = RemoteSensorSource.TalonFX_SelectedSensor; //Remote Source Type
-//
-///* Now that the Left sensor can be used by the master Talon,
-// * set up the Left (Aux) and Right (Master) distance into a single
-// * Robot distance as the Master's Selected Sensor 0. */
-//setRobotDistanceConfigs(_rightInvert, _rightConfig);
-//
-///* FPID for Distance */
-//_rightConfig.slot0.kF = DriveTrainConstants.kGains_Distanc.kF;
-//_rightConfig.slot0.kP = DriveTrainConstants.kGains_Distanc.kP;
-//_rightConfig.slot0.kI = DriveTrainConstants.kGains_Distanc.kI;
-//_rightConfig.slot0.kD = DriveTrainConstants.kGains_Distanc.kD;
-//_rightConfig.slot0.integralZone = DriveTrainConstants.kGains_Distanc.kIzone;
-//_rightConfig.slot0.closedLoopPeakOutput = DriveTrainConstants.kGains_Distanc.kPeakOutput;
-//
-//
-//
-///** Heading Configs */
-//_rightConfig.remoteFilter1.remoteSensorDeviceID = _pidgey.getDeviceID();    //Pigeon Device ID
-//_rightConfig.remoteFilter1.remoteSensorSource = RemoteSensorSource.Pigeon_Yaw; //This is for a Pigeon over CAN
-//_rightConfig.auxiliaryPID.selectedFeedbackSensor = FeedbackDevice.RemoteSensor1; //Set as the Aux Sensor
-//_rightConfig.auxiliaryPID.selectedFeedbackCoefficient = 3600.0 / DriveTrainConstants.kPigeonUnitsPerRotation; //Convert Yaw to tenths of a degree
-//
-///* false means talon's local output is PID0 + PID1, and other side Talon is PID0 - PID1
-// *   This is typical when the master is the right Talon FX and using Pigeon
-// * 
+//** Config Objects for motor controllers */
+TalonFXConfiguration _leftConfig = new TalonFXConfiguration();
+TalonFXConfiguration _rightConfig = new TalonFXConfiguration();
+
+//* Set Neutral Mode */
+MotorL1.setNeutralMode(NeutralMode.Brake);
+MotorR1.setNeutralMode(NeutralMode.Brake);
+
+//* Configure output and sensor direction */
+MotorL1.setInverted(_leftInvert);
+MotorR1.setInverted(_rightInvert);
+
+
+//* Reset Pigeon Configs */
+_pidgey.configFactoryDefault();
+
+
+//** Feedback Sensor Configuration */
+
+//** Distance Configs */
+
+//* Configure the left Talon's selected sensor as integrated sensor */
+_leftConfig.primaryPID.selectedFeedbackSensor = FeedbackDevice.IntegratedSensor; //Local Feedback Source
+
+//* Configure the Remote (Left) Talon's selected sensor as a remote sensor for the right Talon */
+_rightConfig.remoteFilter0.remoteSensorDeviceID = MotorL1.getDeviceID(); //Device ID of Remote Source
+_rightConfig.remoteFilter0.remoteSensorSource = RemoteSensorSource.TalonFX_SelectedSensor; //Remote Source Type
+
+//* Now that the Left sensor can be used by the master Talon,
+//* set up the Left (Aux) and Right (Master) distance into a single
+ //* Robot distance as the Master's Selected Sensor 0. */
+setRobotDistanceConfigs(_rightInvert, _rightConfig);
+
+//* FPID for Distance */
+_rightConfig.slot0.kF = DriveTrainConstants.kGains_Distanc.kF;
+_rightConfig.slot0.kP = DriveTrainConstants.kGains_Distanc.kP;
+_rightConfig.slot0.kI = DriveTrainConstants.kGains_Distanc.kI;
+_rightConfig.slot0.kD = DriveTrainConstants.kGains_Distanc.kD;
+_rightConfig.slot0.integralZone = DriveTrainConstants.kGains_Distanc.kIzone;
+_rightConfig.slot0.closedLoopPeakOutput = DriveTrainConstants.kGains_Distanc.kPeakOutput;
+
+
+
+//** Heading Configs */
+_rightConfig.remoteFilter1.remoteSensorDeviceID = _pidgey.getDeviceID();    //Pigeon Device ID
+_rightConfig.remoteFilter1.remoteSensorSource = RemoteSensorSource.Pigeon_Yaw; //This is for a Pigeon over CAN
+_rightConfig.auxiliaryPID.selectedFeedbackSensor = FeedbackDevice.RemoteSensor1; //Set as the Aux Sensor
+_rightConfig.auxiliaryPID.selectedFeedbackCoefficient = 3600.0 / DriveTrainConstants.kPigeonUnitsPerRotation; //Convert Yaw to tenths of a degree
+
+//* false means talon's local output is PID0 + PID1, and other side Talon is PID0 - PID1
+// *   This is typical when the master is the right Talon FX and using Pigeon */ 
 // * true means talon's local output is PID0 - PID1, and other side Talon is PID0 + PID1
 // *   This is typical when the master is the left Talon FX and using Pigeon
-// */
-//_rightConfig.auxPIDPolarity = false;
-//
-///* FPID for Heading */
-//_rightConfig.slot1.kF = DriveTrainConstants.kGains_Turning.kF;
-//_rightConfig.slot1.kP = DriveTrainConstants.kGains_Turning.kP;
-//_rightConfig.slot1.kI = DriveTrainConstants.kGains_Turning.kI;
-//_rightConfig.slot1.kD = DriveTrainConstants.kGains_Turning.kD;
-//_rightConfig.slot1.integralZone =DriveTrainConstants.kGains_Turning.kIzone;
-//_rightConfig.slot1.closedLoopPeakOutput =DriveTrainConstants.kGains_Turning.kPeakOutput;
-//
-//
-///* Config the neutral deadband. */
-//_leftConfig.neutralDeadband =DriveTrainConstants.kNeutralDeadband;
-//_rightConfig.neutralDeadband =DriveTrainConstants.kNeutralDeadband;
-//
-//
-///**
+
+
+_rightConfig.auxPIDPolarity = false;
+//* FPID for Heading */
+_rightConfig.slot1.kF = DriveTrainConstants.kGains_Turning.kF;
+_rightConfig.slot1.kP = DriveTrainConstants.kGains_Turning.kP;
+_rightConfig.slot1.kI = DriveTrainConstants.kGains_Turning.kI;
+_rightConfig.slot1.kD = DriveTrainConstants.kGains_Turning.kD;
+_rightConfig.slot1.integralZone =DriveTrainConstants.kGains_Turning.kIzone;
+_rightConfig.slot1.closedLoopPeakOutput =DriveTrainConstants.kGains_Turning.kPeakOutput;
+
+
+//* Config the neutral deadband. */
+_leftConfig.neutralDeadband =DriveTrainConstants.kNeutralDeadband;
+_rightConfig.neutralDeadband =DriveTrainConstants.kNeutralDeadband;
+
+
+//**
 // * 1ms per loop.  PID loop can be slowed down if need be.
 // * For example,
 // * - if sensor updates are too slow
 // * - sensor deltas are very small per update, so derivative error never gets large enough to be useful.
 // * - sensor movement is very slow causing the derivative error to be near zero.
 // */
-//int closedLoopTimeMs = 1;
-//MotorR1.configClosedLoopPeriod(0, closedLoopTimeMs,DriveTrainConstants.kTimeoutMs);
-//MotorR1.configClosedLoopPeriod(1, closedLoopTimeMs,DriveTrainConstants.kTimeoutMs);
-//
-///* Motion Magic Configs */
-//_rightConfig.motionAcceleration = 2000; //(distance units per 100 ms) per second
-//_rightConfig.motionCruiseVelocity = 2000; //distance units per 100 ms
-//
-//
-//
-///* APPLY the config settings */
-//MotorL1.configAllSettings(_leftConfig);
-//MotorR1.configAllSettings(_rightConfig);
-//
-//
-///* Set status frame periods to ensure we don't have stale data */
-///* These aren't configs (they're not persistant) so we can set these after the configs.  */
-//MotorR1.setStatusFramePeriod(StatusFrame.Status_12_Feedback1, 20,DriveTrainConstants.kTimeoutMs);
-//MotorR1.setStatusFramePeriod(StatusFrame.Status_13_Base_PIDF0, 20,DriveTrainConstants.kTimeoutMs);
-//MotorR1.setStatusFramePeriod(StatusFrame.Status_14_Turn_PIDF1, 20,DriveTrainConstants.kTimeoutMs);
-//MotorR1.setStatusFramePeriod(StatusFrame.Status_10_Targets, 10,DriveTrainConstants.kTimeoutMs);
-//MotorL1.setStatusFramePeriod(StatusFrame.Status_2_Feedback0, 5,DriveTrainConstants.kTimeoutMs);
-//_pidgey.setStatusFramePeriod(PigeonIMU_StatusFrame.CondStatus_9_SixDeg_YPR , 5,DriveTrainConstants.kTimeoutMs);
+int closedLoopTimeMs = 1;
+MotorR1.configClosedLoopPeriod(0, closedLoopTimeMs,DriveTrainConstants.kTimeoutMs);
+MotorR1.configClosedLoopPeriod(1, closedLoopTimeMs,DriveTrainConstants.kTimeoutMs);
+
+//* Motion Magic Configs */
+_rightConfig.motionAcceleration = 2000; //(distance units per 100 ms) per second
+_rightConfig.motionCruiseVelocity = 2000; //distance units per 100 ms
+
+
+
+//* APPLY the config settings */
+MotorL1.configAllSettings(_leftConfig);
+MotorR1.configAllSettings(_rightConfig);
+
+
+//* Set status frame periods to ensure we don't have stale data */
+//* These aren't configs (they're not persistant) so we can set these after the configs.  */
+MotorR1.setStatusFramePeriod(StatusFrame.Status_12_Feedback1, 20,DriveTrainConstants.kTimeoutMs);
+MotorR1.setStatusFramePeriod(StatusFrame.Status_13_Base_PIDF0, 20,DriveTrainConstants.kTimeoutMs);
+MotorR1.setStatusFramePeriod(StatusFrame.Status_14_Turn_PIDF1, 20,DriveTrainConstants.kTimeoutMs);
+MotorR1.setStatusFramePeriod(StatusFrame.Status_10_Targets, 10,DriveTrainConstants.kTimeoutMs);
+MotorL1.setStatusFramePeriod(StatusFrame.Status_2_Feedback0, 5,DriveTrainConstants.kTimeoutMs);
+_pidgey.setStatusFramePeriod(PigeonIMU_StatusFrame.CondStatus_9_SixDeg_YPR , 5,DriveTrainConstants.kTimeoutMs);
 }
 
 
